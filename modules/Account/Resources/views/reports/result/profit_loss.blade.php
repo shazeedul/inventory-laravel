96317b767cc399af205d71c974797c7737231d97
Syed Shazeedul Islam <syedshazeedul@gmail.com>
2024-08-06 00:33:03 +0600

    profit loss

--- modules/Account/Resources/views/reports/result/profit_loss.blade.php
+++ modules/Account/Resources/views/reports/result/profit_loss.blade.php
@@ -20,14 +20,60 @@
                         <td class="text-end"></td>
                         <td class="text-end">{{ $level_two_income->balance }}</td>
                     </tr>
-                    @empty
-                    @endforelse
-                </tbody>
-                <tfoot>
+                    @foreach ($level_two_income->thirdChild as $level_three_income)
+                        <tr>
+                            <td style="padding-left:100px;">{{ $level_three_income->name }}</td>
+                            <td class="text-end">
+                                {{ $level_three_income->fourthChild->sum('balance') }}
+                            </td>
+                            <td class="text-end"></td>
+                        </tr>
+                    @endforeach
+                @endforeach
+                <tr>
+                    <th class="text-end"></th>
+                    <th class="text-end">@localize('Total Income')</th>
+                    <th class="text-end">{{ $incomeBalance }}</th>
+                </tr>
+                <tr>
+                    <td>@localize('Expense')</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                @foreach ($expenses as $level_two_expense)
                     <tr>
-                        <td class="text-center">@localize('Total')</td>
+                        <td>{{ $level_two_expense->name }}</td>
+                        <td class="text-end"></td>
+                        <td class="text-end">{{ $level_two_expense->balance }}</td>
                     </tr>
-                </tfoot>
-            </table>
-        </div>
+                    @foreach ($level_two_expense->thirdChild as $level_three_expense)
+                        <tr>
+                            <td style="padding-left:100px;">{{ $level_three_expense->name }}</td>
+                            <td class="text-end">{{ $level_three_expense->fourthChild->sum('balance') }}</td>
+                            <td class="text-end"></td>
+                        </tr>
+                    @endforeach
+                @endforeach
+                <tr>
+                    <th class="text-end"></th>
+                    <th class="text-end">@localize('Total Expense')</th>
+                    <th class="text-end">{{ $expenseBalance }}</th>
+                </tr>
+            </tbody>
+            <tfoot>
+                @if ($netProfit >= 0)
+                    <tr>
+                        <th class="text-end">@localize('Net Profit')</th>
+                        <th class="text-end"> {{ $netProfit }}</th>
+                        <th></th>
+                    </tr>
+                @endif
+                <tr>
+                    <th class="text-end"></th>
+                    <th class="text-end">@localize('Total')</th>
+                    <th class="text-end">{{ $netProfit > 0 ? $netProfit : $netLoss }}</th>
+                </tr>
+            </tfoot>
+        </table>
     </div>
+</div>
