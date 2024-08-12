82674dad29474a7727b37cf1805e8bf443b5ae1a
shazeedul <syedshazeedul@gmail.com>
2024-08-12 19:35:27 +0600

    Function migration add

---
+++ modules/Account/Database/Migrations/2024_08_12_103431_create_get_account_nature_function.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+            CREATE FUNCTION GetAccountNature(chart_of_account_id INT)
+            RETURNS VARCHAR(2)
+            DETERMINISTIC
+            BEGIN
+                DECLARE nature VARCHAR(2);
+
+                SELECT 
+                    CASE 
+                        WHEN account_type_id = 1 THEN "DR"
+                        WHEN account_type_id = 2 THEN "CR"
+                        WHEN account_type_id = 3 THEN "CR"
+                        WHEN account_type_id = 4 THEN "DR"
+                        WHEN account_type_id = 5 THEN "CR"
+                        ELSE ""
+                    END INTO nature
+                FROM chart_of_accounts
+                WHERE id = chart_of_account_id
+                LIMIT 1;
+
+                RETURN nature;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP FUNCTION IF EXISTS GetAccountNature');
+    }
+};
